66a5fef623d921362a912b0fa0d8304ee036d036
Sheldon Kasper <analyticalbadger@gmail.com>
2019-05-02 21:55:09 -0600

    Create main.py

---
+++ Module Project/main.py
@@ -0,0 +1,9 @@
+from list_math import *
+
+list1 = [5, 12, 3, 2, 7, 6, 20, 8]
+
+print (CountList(list1))
+print (SumList(list1))
+print (AverageList(list1))
+print (MaxList(list1))
+print (MinList(list1))
