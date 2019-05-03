448fc38610e82e416a145d6b4aeccce426e5b880
Sheldon Kasper <analyticalbadger@gmail.com>
2019-05-02 21:57:11 -0600

    Create CreateEditDeleteFiles.py

---
+++ CreateEditDeleteFiles.py
@@ -0,0 +1,20 @@
+f = open('myfile.txt', 'w')
+
+with open('myfile.txt', 'r') as f:
+    f.read()
+
+l = ['hi', 'there', 'I', 'am', 'writing', 'in', 'a', 'file']
+
+with open('myfile.txt', 'w') as f:
+    for s in l:
+        f.write(s + '\n')
+
+with open('myfile.txt', 'r') as f:
+    for line in f:
+        print(line, end='')
+
+with open('myfile.txt', 'a') as f:
+    f.write('Bye\n')
+
+import os
+os.remove('myfile.txt')
